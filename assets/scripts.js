28eed2ebeb6656ef0f37b73a3f1582b98b3f3e9e
Joel Soderberg <joel@joelsoderberg.se>
2012-05-14 23:08:18 +0200

    Added admin links in navbar.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,11 @@
 (function($) {
+  $(function() {
+    $.get('/authenticate', function(data) {
+      var s = $('p', $(data)).eq(0).text();
+      if(s === 'You are logged in to LoLa!.') {
+        $('#user-menu').show();
+
+      }
+    });
+  });
 })(jQuery);
